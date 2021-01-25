d7d80e736c9021bdc5141c686d70dbb25f878984
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-25 18:59:37 -0500

    landing init

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Landing = () => {
   return (
     <div className="landing-container">
-      <h1>Landing</h1>
+      <img
+        id="landing_background"
+        alt="sky"
+        src="./imgs/christopher-burn.png"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: 200, x: -50 }}
+        animate={{ opacity: 1, y: 0, x: -50 }}
+        transition={{ ease: "easeInOut", duration: 3, delay: 2 }}
+        id="logo"
+        alt="logo"
+        src="./imgs/ERCC_logo.png"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: 150 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5, delay: 0.5 }}
+        id="mountains_back"
+        alt="back mountains"
+        src="./imgs/fore_mountains_large.png"
+      />
+      <img
+        id="mountains_front"
+        alt="front mountains"
+        src="./imgs/fore_mountains_small.png"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5, delay: 1 }}
+        id="grass_left"
+        alt="left grass"
+        src="./imgs/fore_grass_left.png"
+      />
+      <motion.img
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5, delay: 1 }}
+        id="grass_right"
+        alt="right grass"
+        src="./imgs/fore_grass_right.png"
+      />
     </div>
   );
 };
